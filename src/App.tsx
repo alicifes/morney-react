b8463ec33731d9fdc68b9ec9e1e069a57d91a551
alicifes <635023574@qq.com>
2022-04-22 17:07:26 +0800

    基本完成了布局，但是router有问题

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,91 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react' ;
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from 'react-router-dom';
+import styled from 'styled-components';
+
+
+
+const Wrapper = styled.div`
+  border: 1px solid green;
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+  min-height: 100vh;
+`;
+const Main = styled.div`
+  flex-grow: 1;
+`;
+
+const Nav = styled.div`
+  border: 1px solid red;
+  >ul{
+    display: flex;
+    >li{
+      width: 33.3333%;
+      text-align: center;
+      padding: 16px;
+    }
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Wrapper>
+        <Main>
+          <Switch>
+            <Route exact path="/money">
+              <Money/>
+            </Route>
+            <Route exact path="/tags">
+              <Tags/>
+            </Route>
+            <Route exact path="/statistics">
+              <Statistics/>
+            </Route>
+            <Redirect exact from="/" to="/money"/>
+            <Route path="/*">
+              <NoMatch/>
+            </Route>
+          </Switch>
+        </Main>
+        <Nav>
+          <ul>
+            <li>
+              <Link to="/tags">标签页</Link>
+            </li>
+            <li>
+              <Link to="/money">记账页</Link>
+            </li>
+            <li>
+              <Link to="/statistics">统计页</Link>
+            </li>
+          </ul>
+        </Nav>
+      </Wrapper>
+    </Router>
   );
+}
+
+function Tags() {
+  return (<h2>tag</h2>);
+}
+
+function Money() {
+  return (<h1>Money</h1>);
+}
+
+function Statistics() {
+  return <h2>sta</h2>;
+}
+
+function NoMatch() {
+  return <div>no found</div>;
 }
 
 export default App;
