9ff1f7f14a165489066b1e2fa072f1197bbde703
alicifes <635023574@qq.com>
2022-05-10 20:36:48 +0800

    numberpad wid

--- src/components/Money/NumberPadSection.tsx
+++ src/components/Money/NumberPadSection.tsx
@@ -1,70 +1,8 @@
-import styled from 'styled-components';
 import React, {useState} from 'react';
+import {Wrapper} from './NumberPadSection/Wrapper';
+import {generateOutput} from './NumberPadSection/generateOutput';
 
-const Wrapper = styled.section`
-  > .output {
-    background: #ffffff;
-    font-size: 32px;
-    line-height: 72px;
-    padding: 0 16px;
-    text-align: right;
-    box-shadow: inset 0 -5px 5px -5px rgba(0, 0, 0, 0.25) inset 0 5px 5px -5px rgba(0, 0, 0, 0.25);
-  }
 
-  > .pad {
-    > button {
-      float: left;
-      font-size: 18px;
-      border: none;
-      height: 64px;
-      width: 25%;
-
-      &.ok {
-        float: right;
-        height: 128px;
-      }
-
-      &.zero {
-        width: 50%;
-      }
-
-      &:nth-child(1) {
-        background: #f2f2f2;
-      }
-
-      &:nth-child(2),
-      &:nth-child(5) {
-        background: #E0E0E0;
-      }
-
-      &:nth-child(3),
-      &:nth-child(6),
-      &:nth-child(9) {
-        background: #D3D3D3;
-      }
-
-      &:nth-child(4),
-      &:nth-child(7),
-      &:nth-child(10) {
-        background: #C1C1C1;
-      }
-
-      &:nth-child(8),
-      &:nth-child(11),
-      &:nth-child(13) {
-        background: #B8B8B8;
-      }
-
-      &:nth-child(12) {
-        background: #9A9A9A;
-      }
-
-      &:nth-child(14) {
-        background: #A9A9A9;
-      }
-    }
-  }
-`;
 
 const NumberPadSection: React.FC = () => {
   const [output, _setOutput] = useState('0');
@@ -73,41 +11,14 @@ const NumberPadSection: React.FC = () => {
   };
   const onClickButtonWrapper = (e: React.MouseEvent) => {
     const text = (e.target as HTMLButtonElement).textContent;
-    switch (text) {
-      case '1':
-      case '2':
-      case '3':
-      case '4':
-      case '5':
-      case '6':
-      case '7':
-      case '8':
-      case '9':
-      case '0':
-      case '.':
-        if (text === '.' && (output.indexOf('.') >= 0 || output === '0')) {
-          break;
-        }
-        if (output === '0') {
-          setOutput(text);
-        } else {
-          setOutput(output + text);
-        }
-        break;
-      case '删除':
-        if (output.length === 1) {
-          setOutput('0');
-        } else {
-          setOutput(output.slice(0, -1));
-        }
-        break;
-      case '清空':
-        setOutput('0');
-        break;
-      case 'OK':
-        console.log('ok');
+    if(text===null){return;}
+    if (text === 'OK'){
+      //TO DO
+      return ;
     }
-
+    if('0123456789.'.split('').concat('删除','清空').indexOf(text)>=0){
+      setOutput(generateOutput(text,output))
+    }
   };
   return (<Wrapper>
       <div className="output">
